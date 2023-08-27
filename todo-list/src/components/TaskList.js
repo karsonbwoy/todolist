24d92efbf1648e735b68f5e4c2c725efdb0a5891
karsonbwoy <83002847+karsonbwoy@users.noreply.github.com>
2023-08-27 22:39:41 +0000

    Fixed completed tasks issues.

--- todo-list/src/components/TaskList.js
+++ todo-list/src/components/TaskList.js
@@ -3,49 +3,41 @@ import TaskForm from './TaskForm';
 
 function TaskList() {
   const [tasks, setTasks] = useState([]);
-  const [completedTasks, setCompletedTasks] = useState([]);
 
   const addTask = (task) => {
-    setTasks([...tasks, task]);
+    setTasks([...tasks, { task, completed: false }]);
   };
 
   const handleTaskClick = (index) => {
-    if (completedTasks.includes(index)) {
-      setCompletedTasks(completedTasks.filter((item) => item !== index));
-    } else {
-      setCompletedTasks([...completedTasks, index]);
-    }
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
   };
 
   const handleTaskDelete = (index) => {
-    const newTasks = tasks.filter((_, i) => i !== index);
-    setTasks(newTasks);
-  
-    // Jeśli usuwamy zadanie, należy je również usunąć z listy wykonanych zadań
-    setCompletedTasks(completedTasks.filter((item) => item !== index));
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
   };
-  
 
   return (
     <div>
       <TaskForm addTask={addTask} />
       <h2>Lista zadań</h2>
       <ul>
-      {tasks.map((task, index) => (
-        <li
+        {tasks.map((taskObj, index) => (
+          <li
             key={index}
-            onClick={() => handleTaskClick(index)}
-            className={completedTasks.includes(index) ? 'completed' : ''}
-        >
-            {task}
+            className={taskObj.completed ? 'completed' : ''}
+          >
+            <span onClick={() => handleTaskClick(index)}>
+              {taskObj.task}
+            </span>
             <button onClick={() => handleTaskDelete(index)}>Usuń</button>
-        </li>
-      ))}
-
+          </li>
+        ))}
       </ul>
     </div>
   );
-  
 }
 
 export default TaskList;
